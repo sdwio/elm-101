644cf06af7996beb31125d2977097979e529f10d
sdwio <s.dausend.werner@gmail.com>
2017-11-25 19:37:00 +0100

    [Added] Added names and dates and message types

--- broad-chat/enhanced-web-sockets.elm
+++ broad-chat/enhanced-web-sockets.elm
@@ -1,10 +1,14 @@
 module Main exposing (..)
 
+import Date exposing (..)
+import Date.Format exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Html.Extra exposing (static)
 import Html.Keyed as Keyed
+import Json.Decode as Decode
+import Json.Encode exposing (..)
 import List exposing (reverse)
 import String exposing (length)
 import WebSocket
@@ -23,22 +27,132 @@ main =
 -- MODEL
 
 
+type AppState
+    = NameSelection
+    | Connecting
+    | Connected
+
+
+type alias Naming =
+    { name : String }
+
+
+type alias DisplayedMessage =
+    { from : Maybe String
+    , text : String
+    , class : String
+    , timestamp : Float
+    , id : Int
+    }
+
+
 type alias Model =
     { input : String
-    , name : String
+    , naming : Naming
     , waiting : Bool
-    , messages : List String
+    , messages : List DisplayedMessage
+    , appState : AppState
+    , clientNames : List String
+    }
+
+
+type ServerMessageType
+    = MessageBroadcast { from : String, content : String, time : String, messageId : Int } -- from server
+    | UserList List String -- from server
+
+
+type alias StandardMessage =
+    { id : Int
+    , messageType : String
+    , timestamp : Float
+    }
+
+
+standardMessageDecoder : String -> Result String StandardMessage
+standardMessageDecoder json =
+    Decode.decodeString
+        (Decode.map3 StandardMessage
+            (Decode.field "id" Decode.int)
+            (Decode.field "type" Decode.string)
+            (Decode.field "timestamp" Decode.float)
+        )
+        json
+
+
+type alias ChatMessage =
+    { id : Int
+    , messageType : String
+    , timestamp : Float
+    , text : String
+    , from : String
+    }
+
+
+chatMessageDecoder : String -> Result String ChatMessage
+chatMessageDecoder json =
+    Decode.decodeString
+        (Decode.map5 ChatMessage
+            (Decode.field "id" Decode.int)
+            (Decode.field "type" Decode.string)
+            (Decode.field "timestamp" Decode.float)
+            (Decode.field "text" Decode.string)
+            (Decode.field "from" Decode.string)
+        )
+        json
+
+
+type alias ClientNames =
+    { id : Int
+    , messageType : String
+    , timestamp : Float
+    , clientNames : List String
+    }
+
+
+clientNamesDecoder : String -> Result String ClientNames
+clientNamesDecoder json =
+    Decode.decodeString
+        (Decode.map4 ClientNames
+            (Decode.field "id" Decode.int)
+            (Decode.field "type" Decode.string)
+            (Decode.field "timestamp" Decode.float)
+            (Decode.field "clientNames" (Decode.list Decode.string))
+        )
+        json
+
+
+
+--type alias ChatMessageFields =
+--    {id: Int,type: String,timestamp:Int, text: String, from: String}
+-- client messages
+
+
+type alias NewUser =
+    { newUser : String }
+
+
+type alias MessageSent =
+    { from : String, content : String }
+
+
+
+--
+
+
+startingState : Model
+startingState =
+    { input = ""
+    , naming = Naming ""
+    , waiting = False
+    , messages = []
+    , appState = NameSelection
+    , clientNames = []
     }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model "" "" False [], Cmd.none )
-
-
-
--- UPDATE
--- debug comparing with the online example!
+    ( startingState, Cmd.none )
 
 
 type Msg
@@ -46,7 +160,11 @@ type Msg
     | Input String
     | Send
     | NewMessage String
-    | AnotherInt Int
+    | InitializeConnection
+
+
+
+--    | SendName String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -60,33 +178,190 @@ update msg model =
             ( { model | input = newInput }, Cmd.none )
 
         Send ->
-            ( { model | input = "", waiting = True }, WebSocket.send url input )
+            let
+                mObj =
+                    Json.Encode.object
+                        [ ( "type", string "ChatMessageToServer" )
+                        , ( "text", string model.input )
+                        ]
+
+                chatMessage =
+                    encode 0 mObj
+            in
+            ( { model | input = "", waiting = True }, WebSocket.send url chatMessage )
 
         NewMessage str ->
-            ( { model | waiting = False, messages = str :: messages }, Cmd.none )
+            evaluateNewMessage model str
 
         ChangeName str ->
-            ( { model | name = str }, Cmd.none )
+            let
+                naming =
+                    model.naming
+            in
+            ( { model | naming = { naming | name = str } }, Cmd.none )
 
-        AnotherInt int ->
-            ( model, Cmd.none )
+        InitializeConnection ->
+            ( { model | appState = Connecting }, Cmd.none )
 
 
 
+--        SendName message ->
+--            ( { model | appState = Connecting }, WebSocket.send url message )
+
+
+evaluateNewMessage : Model -> String -> ( Model, Cmd Msg )
+evaluateNewMessage model json =
+    let
+        messageTypeString =
+            Decode.decodeString (Decode.field "type" Decode.string) json
+    in
+    case messageTypeString of
+        -- Ok "Info" ->
+        --    justAddJson model json
+        Ok "ChatMessage" ->
+            handleChatMessage model json
+
+        Ok "RequestName" ->
+            sendName model
+
+        Ok "ClientNames" ->
+            setClientNames model json
+
+        _ ->
+            justAddJson model json
+
+
+sendName : Model -> ( Model, Cmd Msg )
+sendName model =
+    let
+        mObj =
+            Json.Encode.object
+                [ ( "type", string "SendName" )
+                , ( "name", string model.naming.name )
+                ]
+
+        message =
+            encode 0 mObj
+    in
+    ( model, WebSocket.send url message )
+
+
+setClientNames : Model -> String -> ( Model, Cmd Msg )
+setClientNames model json =
+    case clientNamesDecoder json of
+        Ok clientNamesMessage ->
+            ( { model
+                | appState = Connected
+                , clientNames = clientNamesMessage.clientNames
+              }
+            , Cmd.none
+            )
+
+        Err string ->
+            let
+                message =
+                    { from = Nothing
+                    , text = json ++ string ++ "ERRÖR"
+                    , class = "standard"
+                    , timestamp = 0
+                    , id = 0
+                    }
+            in
+            ( { model
+                | waiting = False
+                , appState = Connected
+                , messages = message :: model.messages
+              }
+            , Cmd.none
+            )
+
+
+handleChatMessage : Model -> String -> ( Model, Cmd Msg )
+handleChatMessage model json =
+    case chatMessageDecoder json of
+        Ok chatMessage ->
+            let
+                message =
+                    { from = Just chatMessage.from
+                    , text = chatMessage.text
+                    , class = "chat-message"
+                    , timestamp = chatMessage.timestamp
+                    , id = chatMessage.id
+                    }
+            in
+            ( { model
+                | waiting = False
+                , appState = Connected
+                , messages = message :: model.messages
+              }
+            , Cmd.none
+            )
+
+        Err string ->
+            justAddJson model json
+
+
+
+{--
+            type alias DisplayedMessage =
+                { from : Maybe String
+                , text : String
+                , class : String
+                }
+--}
+
+
+justAddJson : Model -> String -> ( Model, Cmd Msg )
+justAddJson model json =
+    let
+        message =
+            { from = Nothing
+            , text = json ++ " DEFAULT"
+            , class = "standard"
+            , timestamp = 0
+            , id = 0
+            }
+    in
+    ( { model
+        | waiting = False
+        , appState = Connected
+        , messages = message :: model.messages
+      }
+    , Cmd.none
+    )
+
+
+
+{--
+    ( { model
+        | waiting = False
+        , appState = Connected
+        , messages = (toString messageType ++ " " ++ json) :: model.messages
+      }
+    , Cmd.none
+    )
+    --}
 -- SUBSCRIPTIONS
+-- for multiple subscriptions
+-- see http://package.elm-lang.org/packages/elm-lang/core/5.1.1/Platform-Sub#batch
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    WebSocket.listen url NewMessage
+    case model.appState of
+        NameSelection ->
+            Sub.none
+
+        _ ->
+            WebSocket.listen url NewMessage
 
 
 
 -- VIEW
 
 
-css : String -> Html Msg
-css route =
+linkCss : String -> Html Never
+linkCss route =
     Html.node "link"
         [ Html.Attributes.rel "stylesheet"
         , Html.Attributes.href route
@@ -94,84 +369,14 @@ css route =
         []
 
 
-type Mix
-    = String
-    | Int
-
-
-foo0 : Int -> Mix
-foo0 int =
-    if int > 9 then
-        String
-    else
-        Int
-
-
-foo4 : Mix -> Int
-foo4 mix =
-    case mix of
-        Int ->
-            1
-
-        String ->
-            0
-
-
-type Mix2
-    = AString String
-    | AnInt Int
-
-
-type Fruit
-    = Apple
-    | Banana
-    | Fish
-
-
-foo : Int -> Fruit
-foo int =
-    if int > 3 then
-        Apple
-    else
-        Banana
-
-
-foo2 : Mix2 -> Int
-foo2 mix =
-    case mix of
-        AnInt int ->
-            2 * int
-
-        AString string ->
-            length string
-
-
-foo3 : Int -> Msg
-foo3 int =
-    if int > 4 then
-        AnotherInt (2 * int)
-    else
-        Send
-
-
-xview : Model -> Html Msg
-xview model =
-    div [] [ text "Never?", static never ]
-
-
-never : Html Never
-never =
-    div [] [ text "abc" ]
-
-
 view : Model -> Html Msg
 view model =
     div []
-        [ css "css/Skeleton-2.0.4/css/skeleton.css"
-        , css "css/normalize.css"
-        , css "https://fonts.googleapis.com/css?family=Encode+Sans"
-        , css "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-        , css "css/screen.css"
+        [ static <| linkCss "css/Skeleton-2.0.4/css/skeleton.css"
+        , static <| linkCss "css/normalize.css"
+        , static <| linkCss "https://fonts.googleapis.com/css?family=Encode+Sans"
+        , static <| linkCss "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        , static <| linkCss "css/screen.css"
         , outerFrame model
         , div []
             (if model.waiting then
@@ -188,10 +393,26 @@ enterYourName model =
         [ input
             [ type_ "text"
             , onInput ChangeName
-            , value model.name
+            , value model.naming.name
             ]
             []
+        , button
+            [ namingIsValid model.naming |> not |> disabled, onClick InitializeConnection ]
+            [ text "jetzt Loslegen!" ]
         ]
+
+
+emptyAttribute =
+    style []
+
+
+
+-- TODO: define a naming format and hand over only model.name
+
+
+namingIsValid : Naming -> Bool
+namingIsValid naming =
+    length naming.name >= 3
 
 
 outerFrame : Model -> Html Msg
@@ -201,12 +422,24 @@ outerFrame model =
             [ h1 []
                 [ i [ class "fa fa-comments-o" ] []
                 , text " Broad-Chat | "
-                , small [] [ text (" hi " ++ model.name) ]
+                , small [] [ text (" hi " ++ model.naming.name) ]
                 ]
             ]
-        , enterYourName model
-        , mainStructure model
+        , selectViewByAppState model
         ]
+
+
+selectViewByAppState : Model -> Html Msg
+selectViewByAppState model =
+    case model.appState of
+        NameSelection ->
+            enterYourName model
+
+        Connecting ->
+            div [] [ text "loading" ]
+
+        Connected ->
+            mainStructure model
 
 
 mainStructure : Model -> Html Msg
@@ -222,8 +455,13 @@ activeUsers model =
     div [ class "active-users" ]
         [ div [ class "active-users-heading" ] [ text "2 active users:" ]
         , div [ class "active-users-list" ]
-            [ ul [] [ li [] [ text "Sam" ], li [] [ text "Mary" ] ] ]
+            [ ul [] [ static <| listClientNames model.clientNames ] ]
         ]
+
+
+listClientNames : List String -> Html Never
+listClientNames clientNames =
+    List.map
 
 
 inputFrame : Model -> Html Msg
@@ -264,33 +502,33 @@ chatEntry message =
         ]
 
 
-chatEntry1 : String -> ( String, Html Msg )
+chatEntry1 : DisplayedMessage -> ( String, Html Msg )
 chatEntry1 message =
-    ( message
+    let
+        from =
+            case message.from of
+                Just from ->
+                    from ++ ": "
+
+                Nothing ->
+                    ""
+
+        time2 =
+            toString message.timestamp
+
+        time =
+            format "%H:%M %d.%m.'%y" (fromTime message.timestamp)
+    in
+    ( toString message.id
     , li [ class "chat-entry" ]
-        [ div [ class "chat-time grey" ] [ text "16.11.'17 12:43" ]
+        [ div [ class "chat-time grey" ] [ text time ]
         , div []
-            [ span [ class "name" ] [ text "Sam: " ]
-            , text message
+            [ span [ class "name" ] [ text from ]
+            , text message.text
             ]
+        , div [] [ text ("id " ++ toString message.id) ]
         ]
     )
-
-
-
-{--<div class="eight columns">
-        <div class="input-frame">
---            <div class="input-element">
---                <label for="main-input">
---                    <i class="fa fa-pencil fa-flip-horizontal"></i>
---                </label>
---                <input class="main-input" id="main-input" type="text" name="main-input" value="" placeholer="">
---            </div>
---            <div class="send-button">
---                <i class="fa fa-paper-plane-o"></i> </div>
---            </div>
---            <div class="chat-history">
---}
 
 
 viewMessage : String -> Html msg
